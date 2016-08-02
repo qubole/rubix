9fad94d9ecafbdef63ef1fdd84c5d8f30b68f687
Qubole <sakshia@qubole.com>
2016-08-02 15:51:35 +0530

    Method in server to return Cache statistics
    Created a new method in server which returns Cache statistics (Cache Hit Rate and Miss Rate).

--- rubix-bookkeeper/src/main/thrift/bookkeeper.thrift
+++ rubix-bookkeeper/src/main/thrift/bookkeeper.thrift
@@ -1,10 +1,14 @@
 namespace java com.qubole.rubix.bookkeeper
+namespace python com.qubole.rubix.bookkeeper
 
 typedef i64 long
+typedef i32 int
 
 service BookKeeperService
 {
     list<bool> getCacheStatus(1:string remotePath, 2:long fileLength, 3:long startBlock, 4:long endBlock)
 
     oneway void setAllCached(1:string remotePath, 2:long fileLength, 3:long startBlock, 4:long endBlock)
+
+    map<string,double> getCacheStats()
 }
