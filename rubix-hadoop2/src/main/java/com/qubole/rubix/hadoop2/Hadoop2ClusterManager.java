bc5f777d09ec3f5379bb2fa7c7a99aed6357d619
sakshia125 <sakshia@qubole.com>
2016-08-09 16:48:09 +0530

    Implement Hadoop-2 extension (#10)
    Created a Hadoop2-extension for rubix

---
+++ rubix-hadoop2/src/main/java/com/qubole/rubix/hadoop2/Hadoop2ClusterManager.java
@@ -0,0 +1,206 @@
+/**
+ * Copyright (c) 2016. Qubole Inc
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
+ */
+package com.qubole.rubix.hadoop2;
+
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.qubole.rubix.core.ClusterManager;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+
+/**
+ * Created by sakshia on 28/7/16.
+ */
+public class Hadoop2ClusterManager
+        extends ClusterManager
+{
+
+    private boolean isMaster = true;
+    public int serverPort = 8088;
+    private String serverAddress = "localhost";
+    private Supplier<List<String>> nodesSupplier;
+    String address = "localhost:8088";
+    private Log log = LogFactory.getLog(Hadoop2ClusterManager.class);
+    static String ADDRESS = "yarn.resourcemanager.webapp.address";
+
+    @Override
+    public void initialize(Configuration conf)
+    {
+        super.initialize(conf);
+        log.debug("Initializing: Hadoop2ClusterManager");
+        this.address = conf.get(ADDRESS, address);
+        this.serverAddress = address.substring(0, address.indexOf(":"));
+        this.serverPort = Integer.parseInt(address.substring(address.indexOf(":") + 1));
+        nodesSupplier = Suppliers.memoizeWithExpiration(new Supplier<List<String>>()
+        {
+
+            @Override
+            public List<String> get()
+            {
+                if (!isMaster) {
+                    // First time all nodes start assuming themselves as master and down the line figure out their role
+                    // Next time onwards, only master will be fetching the list of nodes
+                    return ImmutableList.of();
+                }
+                try {
+                    StringBuffer response = new StringBuffer();
+                    URL obj = getNodeURL();
+                    HttpURLConnection httpcon = (HttpURLConnection) obj.openConnection();
+                    httpcon.setRequestMethod("GET");
+                    int responseCode = httpcon.getResponseCode();
+                    log.debug("Sending 'GET' request to URL: " + obj.toString());
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                        BufferedReader in = new BufferedReader(new InputStreamReader(httpcon.getInputStream()));
+                        String inputLine;
+                        while ((inputLine = in.readLine()) != null) {
+                            response.append(inputLine);
+                        }
+                        in.close();
+                        httpcon.disconnect();
+                    }
+                    else {
+                        log.info("/ws/v1/cluster/nodes failed due to " + responseCode + ". Setting this node as worker.");
+                        isMaster = false;
+                        httpcon.disconnect();
+                        return ImmutableList.of();
+                    }
+                    Gson gson = new Gson();
+                    Type type = new TypeToken<Nodes>() {}.getType();
+                    Nodes nodes = gson.fromJson(response.toString(), type);
+                    List<Elements> allNodes = nodes.getNodes().getNode();
+                    Set<String> hosts = new HashSet<>();
+
+                    for (Elements node : allNodes) {
+                        String state = node.getState();
+                        log.debug("Hostname: " + node.getNodeHostName() + "State: " + state);
+                        //keep only healthy data nodes
+                        if (state.equalsIgnoreCase("Running") || state.equalsIgnoreCase("New") || state.equalsIgnoreCase("Rebooted")) {
+                            hosts.add(node.getNodeHostName());
+                        }
+                    }
+
+                    if (hosts.isEmpty()) {
+                        throw new Exception("No healthy data nodes found.");
+                    }
+
+                    List<String> hostList = Lists.newArrayList(hosts.toArray(new String[0]));
+                    Collections.sort(hostList);
+                    log.debug("Hostlist: " + hostList.toString());
+                    return hostList;
+                }
+                catch (Exception e) {
+                    throw Throwables.propagate(e);
+                }
+            }
+        }, 10, TimeUnit.SECONDS);
+    }
+
+    public URL getNodeURL()
+            throws MalformedURLException
+    {
+        return new URL("http://" + serverAddress + ":" + serverPort + "/ws/v1/cluster/nodes");
+    }
+
+    @Override
+    public boolean isMaster()
+    {
+        // issue get on nodesSupplier to ensure that isMaster is set correctly
+        nodesSupplier.get();
+        return isMaster;
+    }
+
+    @Override
+    public List<String> getNodes()
+    {
+        return nodesSupplier.get();
+    }
+
+    public static class Nodes
+    {
+        public Nodes()
+        {
+        }
+        private Node nodes;
+
+        public void setNodes(Node nodes)
+        {
+            this.nodes = nodes;
+        }
+        public Node getNodes()
+        {
+            return nodes;
+        }
+    }
+
+    public static class Node
+    {
+
+        public Node()
+        {
+        }
+        private List<Elements> node;
+
+        public void setNode(List<Elements> node)
+        {
+            this.node = node;
+        }
+        public List<Elements> getNode()
+        {
+            return node;
+        }
+    }
+
+    public static class Elements
+    {
+        /*
+        rack 	         string
+        state 	         string
+        id 	             string
+        nodeHostName 	 string
+        nodeHTTPAddress  string
+        healthStatus 	 string
+        healthReport 	 string
+        lastHealthUpdate long
+        usedMemoryMB 	 long
+        availMemoryMB 	 long
+        numContainers 	 int
+        */
+
+        String nodeHostName;
+        String state;
+
+        String getState() { return state; }
+
+        String getNodeHostName() { return nodeHostName; }
+    }
+}
---
+++ rubix-hadoop2/src/test/java/com/qubole/rubix/hadoop2/TestClusterManager.java
@@ -0,0 +1,124 @@
+/**
+ * Copyright (c) 2016. Qubole Inc
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
+ */
+package com.qubole.rubix.hadoop2;
+
+import com.qubole.rubix.core.ClusterManager;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.testng.annotations.Test;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.BindException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.List;
+
+import static org.testng.AssertJUnit.assertTrue;
+import static org.testng.AssertJUnit.fail;
+
+/**
+ * Created by sakshia on 1/8/16.
+ */
+@Test(singleThreaded = true)
+public class TestClusterManager
+{
+    private static final Log LOG = LogFactory.getLog(TestClusterManager.class);
+
+    //Returns only healthy data nodes (Not master)
+    @Test
+    public void testGetNodes()
+            throws IOException
+    {
+        HttpServer server = startServer(new MultipleNodeHandler());
+        LOG.debug("Started Server");
+        ClusterManager cm = getHadoop2ClusterManager();
+        List<String> nodes = cm.getNodes();
+        LOG.debug("Got nodes: " + nodes);
+        assertTrue("Should have 2 nodes", nodes.size() == 2);
+        server.stop(0);
+    }
+
+
+    @Test
+    public void testFailOnAllNodesUnhealthy()
+            throws IOException
+    {
+        HttpServer server = startServer(new FailedNodeHandler());
+        LOG.debug("Started Server");
+        try{
+            ClusterManager cm = getHadoop2ClusterManager();
+            List<String> nodes = cm.getNodes();
+            fail("Expected exception : No healthy data nodes found.");
+        }
+        catch (Exception e){
+            LOG.debug("Caught Exception: " + e.getMessage());
+        }
+
+        server.stop(0);
+    }
+
+
+    private ClusterManager getHadoop2ClusterManager()
+            throws IOException
+    {
+
+            ClusterManager clusterManager = new Hadoop2ClusterManager();
+            Configuration conf = new Configuration();
+            conf.set(Hadoop2ClusterManager.ADDRESS, "localhost:45326");
+            clusterManager.initialize(conf);
+            return clusterManager;
+    }
+
+    private HttpServer startServer(HttpHandler handler)
+            throws IOException
+    {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(45326), 0);
+        server.createContext("/ws/v1/cluster/nodes", handler);
+        server.setExecutor(null); // creates a default executor
+        server.start();
+        return server;
+    }
+
+    class MultipleNodeHandler implements HttpHandler
+    {
+        public void handle(HttpExchange exchange) throws IOException {
+            String nodes = String.format("{\"nodes\":{\"node\":[{\"rack\":\"\\/default-rack\",\"state\":\"NEW\",\"id\":\"h2:1235\",\"nodeHostName\":\"h2\",\"nodeHTTPAddress\":\"h2:2\",\"healthStatus\":\"Unhealthy\",\"lastHealthUpdate\":1324056895432,\"healthReport\":\"Healthy\",\"numContainers\":0,\"usedMemoryMB\":0,\"availMemoryMB\":8192,\"usedVirtualCores\":0,\"availableVirtualCores\":8},{\"rack\":\"\\/default-rack\",\"state\":\"NEW\",\"id\":\"h2:1235\",\"nodeHostName\":\"%s\",\"nodeHTTPAddress\":\"h2:2\",\"healthStatus\":\"Healthy\",\"lastHealthUpdate\":1324056895432,\"healthReport\":\"Healthy\",\"numContainers\":0,\"usedMemoryMB\":0,\"availMemoryMB\":8192,\"usedVirtualCores\":0,\"availableVirtualCores\":8},{\"rack\":\"\\/default-rack\",\"state\":\"Unhealthy\",\"id\":\"h1:1234\",\"nodeHostName\":\"h1\",\"nodeHTTPAddress\":\"h1:2\",\"healthStatus\":\"Healthy\",\"lastHealthUpdate\":1324056895092,\"healthReport\":\"Healthy\",\"numContainers\":0,\"usedMemoryMB\":0,\"availMemoryMB\":8192,\"usedVirtualCores\":0,\"availableVirtualCores\":8}]}}",InetAddress.getLocalHost().getHostName());
+            exchange.getResponseHeaders().add("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, nodes.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(nodes.getBytes());
+            os.close();
+        }
+    }
+
+
+    class FailedNodeHandler implements HttpHandler
+    {
+        public void handle(HttpExchange exchange) throws IOException {
+            String nodes = String.format("{\"nodes\":{\"node\":[{\rack\":\"\\/default-rack\",\"state\":\"Unhealthy\",\"id\":\"h2:1235\",\"nodeHostName\":\"%s\",\"nodeHTTPAddress\":\"h2:2\",\"healthStatus\":\"Healthy\",\"lastHealthUpdate\":1324056895432,\"healthReport\":\"Healthy\",\"numContainers\":0,\"usedMemoryMB\":0,\"availMemoryMB\":8192,\"usedVirtualCores\":0,\"availableVirtualCores\":8},{\"rack\":\"\\/default-rack\",\"state\":\"Unhealthy\",\"id\":\"h1:1234\",\"nodeHostName\":\"h1\",\"nodeHTTPAddress\":\"h1:2\",\"healthStatus\":\"Healthy\",\"lastHealthUpdate\":1324056895092,\"healthReport\":\"Healthy\",\"numContainers\":0,\"usedMemoryMB\":0,\"availMemoryMB\":8192,\"usedVirtualCores\":0,\"availableVirtualCores\":8}]}}",InetAddress.getLocalHost().getHostName());
+            exchange.getResponseHeaders().add("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, nodes.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(nodes.getBytes());
+            os.close();
+        }
+    }
+
+
+}
+
