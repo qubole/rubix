ac6942ca9da3d15cdc8991249b1f663fcd290a1b
Abhishek Das <adas@qubole.com>
2017-10-16 20:39:18 -0700

    adding support for rubix in open source presto (emr)

--- rubix-rpm/src/main/resources/rpm/scripts/configure.sh
+++ rubix-rpm/src/main/resources/rpm/scripts/configure.sh
@@ -2,7 +2,11 @@
 #configure bookkeeper server.
 INPUT_FILE="/usr/lib/hadoop/etc/hadoop/core-site.xml"
 sed -i '$ i <property>\n<name>hadoop.cache.data.dirprefix.list</name>\n<value>/var/lib/rubix/cache/data</value>\n</property>' $INPUT_FILE
+sed -i '$ i <property>\n<name>fs.rubix.impl</name>\n<value>com.qubole.rubix.presto.CachingPrestoS3FileSystem</value>\n</property>' $INPUT_FILE
+sed -i '$ i <property>\n<name>caching.fs.presto-server-port</name>\n<value>8889</value>\n</property>' $INPUT_FILE
 
+host_name=$(grep --no-group-separator -a2 "yarn.resourcemanager.hostname" /usr/lib/hadoop/etc/hadoop/yarn-site.xml | sed  "s/yarn.resourcemanager.hostname/master.hostname/g" | tr -d ' ' | tr -d '\n')
+sed -i '$i'$host_name'' $INPUT_FILE
 
 #configure client
 OUTPUT_FILE="/etc/rubix/config.xml"
@@ -17,6 +21,10 @@ OUTPUT_FILE="/etc/rubix/config.xml"
   <name>fs.rubix.impl</name>
   <value>com.qubole.rubix.presto.CachingPrestoS3FileSystem</value>
   <description>http://wiki.apache.org/hadoop/Hive/HBaseIntegration</description>
+</property>
+<property>
+  <name>caching.fs.presto-server-port</name>
+  <value>8889</value>
 </property>
 
 EOF
