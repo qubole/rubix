f0e354d72ee4d1eed7170239c41b971ddc4710e9
Abhishek Das <adas@qubole.com>
2017-10-23 01:44:45 -0700

    adding support for rubix in open source presto (emr)

--- rubix-rpm/src/main/resources/rpm/scripts/configure.sh
+++ rubix-rpm/src/main/resources/rpm/scripts/configure.sh
@@ -2,7 +2,6 @@
 #configure bookkeeper server.
 INPUT_FILE="/usr/lib/hadoop/etc/hadoop/core-site.xml"
 sed -i '$ i <property>\n<name>hadoop.cache.data.dirprefix.list</name>\n<value>/var/lib/rubix/cache/data</value>\n</property>' $INPUT_FILE
-sed -i '$ i <property>\n<name>fs.rubix.impl</name>\n<value>com.qubole.rubix.presto.CachingPrestoS3FileSystem</value>\n</property>' $INPUT_FILE
 sed -i '$ i <property>\n<name>caching.fs.presto-server-port</name>\n<value>8889</value>\n</property>' $INPUT_FILE
 
 host_name=$(grep --no-group-separator -a2 "yarn.resourcemanager.hostname" /usr/lib/hadoop/etc/hadoop/yarn-site.xml | sed  "s/yarn.resourcemanager.hostname/master.hostname/g" | tr -d ' ' | tr -d '\n')
@@ -32,3 +31,9 @@ EOF
 
 grep --no-group-separator -a2 "yarn.resourcemanager.address" /usr/lib/hadoop/etc/hadoop/yarn-site.xml >> $OUTPUT_FILE
 echo "</configuration>" >> $OUTPUT_FILE
+
+#Configuring Presto Server to include /rubix/config.xml as resource
+PRESTO_JVM_CONFIG_FILE="/etc/presto/conf/jvm.config"
+config_string="\-Dhive\.config\.resources\="
+rubix_config_path=",\/etc\/rubix\/config.xml"
+sed -i "/^$config_string/s/$/$rubix_config_path/" $PRESTO_JVM_CONFIG_FILE
