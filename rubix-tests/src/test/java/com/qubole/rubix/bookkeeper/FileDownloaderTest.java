ffca26b4c3d6ea26365c4858c6e7b6dfc6604df2
Abhishek Das <adas@qubole.com>
2018-03-12 16:58:22 -0700

    added unit test for FileDownloader

---
+++ rubix-tests/src/test/java/com/qubole/rubix/bookkeeper/FileDownloaderTest.java
@@ -0,0 +1,59 @@
+package com.qubole.rubix.bookkeeper;
+
+/**
+ * Copyright (c) 2016. Qubole Inc
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
+ */
+
+import com.qubole.rubix.core.FileDownloadRequestChain;
+import org.apache.hadoop.conf.Configuration;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+/**
+ * Created by Abhishek on 3/12/18.
+ */
+public class FileDownloaderTest {
+
+  @Test
+  public void testGetFileDownloadRequestChains() throws Exception {
+
+    ConcurrentMap<String, DownloadRequestContext> contextMap = new ConcurrentHashMap<String, DownloadRequestContext>();
+    DownloadRequestContext context = new DownloadRequestContext("file:///Files/file-1", 1000, 1000);
+    contextMap.put("file:///Files/file-1", context);
+    context.addDownloadRange(100, 300);
+    context.addDownloadRange(250, 400);
+    context.addDownloadRange(500, 800);
+
+    context = new DownloadRequestContext("file:///Files/file-2", 1000, 1000);
+    contextMap.put("file:///Files/file-2", context);
+    context.addDownloadRange(100, 200);
+    context.addDownloadRange(500, 800);
+
+    FileDownloader downloader = new FileDownloader(new Configuration());
+    List<FileDownloadRequestChain> requestChains = downloader.getFileDownloadRequestChains(contextMap);
+
+    assertTrue("Wrong Number of Request Chains. Expected = 2 Got = " + requestChains.size(), requestChains.size() == 2);
+
+    FileDownloadRequestChain request1 = requestChains.get(0);
+    assertTrue("Wrong Number of Requests For File-1. Expected = 2 Got = " + request1.getReadRequests().size(),
+        request1.getReadRequests().size() == 2);
+
+    FileDownloadRequestChain request2 = requestChains.get(1);
+    assertTrue("Wrong Number of Requests For File-2. Expected = 2 Got = " + request2.getReadRequests().size(),
+        request2.getReadRequests().size() == 2);
+  }
+}
