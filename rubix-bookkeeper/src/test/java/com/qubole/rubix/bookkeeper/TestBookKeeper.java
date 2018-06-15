ad897f97763dff7b1e752accaf6f361b6f6cfdb4
Abhishek Das <adas@qubole.com>
2018-06-15 01:25:47 -0700

    Fixed unit tests

--- rubix-bookkeeper/src/test/java/com/qubole/rubix/bookkeeper/TestBookKeeper.java
+++ rubix-bookkeeper/src/test/java/com/qubole/rubix/bookkeeper/TestBookKeeper.java
@@ -14,6 +14,7 @@ package com.qubole.rubix.bookkeeper;
 
 import com.codahale.metrics.MetricRegistry;
 import com.qubole.rubix.core.ClusterManagerInitilizationException;
+import com.qubole.rubix.core.utils.DeleteFileVisitor;
 import com.qubole.rubix.core.utils.DummyClusterManager;
 import com.qubole.rubix.spi.CacheConfig;
 import com.qubole.rubix.spi.ClusterManager;
@@ -23,32 +24,50 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static org.testng.Assert.assertTrue;
 
 /**
  * Created by Abhishek on 6/15/18.
  */
-public class BookKeeperTest
+public class TestBookKeeper
 {
+  private MetricRegistry metrics;
+  private Configuration conf;
+
   @BeforeMethod
   public void setUp() throws Exception
   {
+    conf = new Configuration();
+    metrics = new MetricRegistry();
+
+    // Set configuration values for testing
+    CacheConfig.setCacheDataDirPrefix(conf, "/tmp/media/ephemeral");
+    CacheConfig.setMaxDisks(conf, 1);
+
+    // Create cache directories
+    Files.createDirectories(Paths.get(CacheConfig.getCacheDirPrefixList(conf)));
+    for (int i = 0; i < CacheConfig.getCacheMaxDisks(conf); i++) {
+      Files.createDirectories(Paths.get(CacheConfig.getCacheDirPrefixList(conf) + i));
+    }
   }
 
   @AfterMethod
   public void tearDown() throws Exception
   {
+    for (int i = 0; i < CacheConfig.getCacheMaxDisks(conf); i++) {
+      Files.walkFileTree(Paths.get(CacheConfig.getCacheDirPrefixList(conf) + i), new DeleteFileVisitor());
+      Files.deleteIfExists(Paths.get(CacheConfig.getCacheDirPrefixList(conf) + i));
+    }
   }
 
   @Test
   public void testGetClusterManagerValidInstance() throws Exception
   {
-    Configuration conf = new Configuration();
     ClusterType type = ClusterType.TEST_CLUSTER_MANAGER;
-    MetricRegistry metrics = new MetricRegistry();
-
     BookKeeper bookKeeper = new CoordinatorBookKeeper(conf, metrics);
-
     ClusterManager manager = bookKeeper.getClusterManagerInstance(type, conf);
 
     assertTrue(manager instanceof DummyClusterManager, " Didn't initialize the correct cluster manager class." +
@@ -58,10 +77,7 @@ public class BookKeeperTest
   @Test(expectedExceptions = ClusterManagerInitilizationException.class)
   public void testGetClusterManagerInValidInstance() throws Exception
   {
-    Configuration conf = new Configuration();
     ClusterType type = ClusterType.TEST_CLUSTER_MANAGER;
-    MetricRegistry metrics = new MetricRegistry();
-
     CacheConfig.setDummyClusterManager(conf, "com.qubole.rubix.core.DoesNotExistClusterManager");
     BookKeeper bookKeeper = new CoordinatorBookKeeper(conf, metrics);
 
