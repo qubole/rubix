a3cf13e559862bd68a4590eea69e98b1385a93ed
Jordan Wilson <jordanw@bitquilltech.com>
2018-08-08 11:28:01 -0700

    chg: dev: #164: fix rubix-common jar path in Docker build script

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ HADOOP_JARSPATH="/usr/lib/hadoop2/share/hadoop/tools/lib/"
 
 RUBIX_SPI_JAR="${PWD}/rubix-spi/target/rubix-spi-*SNAPSHOT.jar"
 RUBIX_CORE_JAR="${PWD}/rubix-core/target/rubix-core-*SNAPSHOT.jar"
-RUBIX_COMMON_JAR="${PWD}/rubix-core/target/rubix-common-*SNAPSHOT.jar"
+RUBIX_COMMON_JAR="${PWD}/rubix-common/target/rubix-common-*SNAPSHOT.jar"
 RUBIX_PRESTO_JAR="${PWD}/rubix-presto/target/rubix-presto-*SNAPSHOT.jar"
 RUBIX_HADOOP2_JAR="${PWD}/rubix-hadoop2/target/rubix-hadoop2-*SNAPSHOT.jar"
 RUBIX_BOOKKEEPER_JAR="${PWD}/rubix-bookkeeper/target/rubix-bookkeeper-*SNAPSHOT.jar"
