e0e642a7af3edb9d649be8f0e966144ef24eb4bf
Jordan Wilson <jordanw@bitquilltech.com>
2019-01-23 17:12:44 -0800

    chg: usr: #256: specify Jets3t JAR instead of entire Hadoop lib dir

--- rubix-rpm/src/main/resources/rpm/scripts/configure-spark.sh
+++ rubix-rpm/src/main/resources/rpm/scripts/configure-spark.sh
@@ -7,11 +7,11 @@ SPARK_DEFAULTS_CONF_FILE="/etc/spark/conf/spark-defaults.conf"
 SPARK_DRIVER_EXTRA_CP_OPTION="spark\.driver\.extraClassPath"
 SPARK_EXECUTOR_EXTRA_CP_OPTION="spark\.executor\.extraClassPath"
 RUBIX_EXTRA_CP=":${RUBIX_HOME_DIR}/lib/*"
-HADOOP_EXTRA_CP=":/usr/lib/hadoop/lib/*"
+JETS3T_EXTRA_CP=":/usr/lib/hadoop/lib/jets3t-0.9.0.jar"
 sed -i "/^$SPARK_DRIVER_EXTRA_CP_OPTION/ s|$|$RUBIX_EXTRA_CP|" ${SPARK_DEFAULTS_CONF_FILE}
-sed -i "/^$SPARK_DRIVER_EXTRA_CP_OPTION/ s|$|$HADOOP_EXTRA_CP|" ${SPARK_DEFAULTS_CONF_FILE}
+sed -i "/^$SPARK_DRIVER_EXTRA_CP_OPTION/ s|$|$JETS3T_EXTRA_CP|" ${SPARK_DEFAULTS_CONF_FILE}
 sed -i "/^$SPARK_EXECUTOR_EXTRA_CP_OPTION/ s|$|$RUBIX_EXTRA_CP|" ${SPARK_DEFAULTS_CONF_FILE}
-sed -i "/^$SPARK_EXECUTOR_EXTRA_CP_OPTION/ s|$|$HADOOP_EXTRA_CP|" ${SPARK_DEFAULTS_CONF_FILE}
+sed -i "/^$SPARK_EXECUTOR_EXTRA_CP_OPTION/ s|$|$JETS3T_EXTRA_CP|" ${SPARK_DEFAULTS_CONF_FILE}
 
 echo "spark.hadoop.fs.s3.impl   com.qubole.rubix.hadoop2.CachingNativeS3FileSystem" >> ${SPARK_DEFAULTS_CONF_FILE}
 echo "spark.hadoop.fs.s3n.impl  com.qubole.rubix.hadoop2.CachingNativeS3FileSystem" >> ${SPARK_DEFAULTS_CONF_FILE}
