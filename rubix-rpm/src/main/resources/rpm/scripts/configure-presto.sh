b42dac2dd01c09004c2ec3bcddb60980e7947d4d
Jordan Wilson <37088125+jordanw-bq@users.noreply.github.com>
2019-02-01 11:29:10 -0800

    new: usr: #256: Add RPM support for other cluster types (#257)

---
+++ rubix-rpm/src/main/resources/rpm/scripts/configure-presto.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+RUBIX_HOME_DIR=/usr/lib/rubix
+RUBIX_CACHE_DIR=/var/lib/rubix/cache
+PRESTO_HOME_DIR=/usr/lib/presto
+HADOOP_HOME_DIR=/usr/lib/hadoop
+
+# Configure BookKeeper server for PrestoClusterManager
+RUBIX_SITE=/etc/rubix/rubix-site.xml
+sed -i '$ i <property>\n<name>caching.fs.presto-server-port</name>\n<value>8889</value>\n</property>' ${RUBIX_SITE}
+
+# Configure Presto as RubiX client
+RUBIX_PRESTO_CLIENT_CONFIG="/etc/rubix/presto-config.xml"
+(cat <<CLIENT
+<?xml version="1.0"?>
+<configuration>
+    <property>
+        <name>hadoop.cache.data.dirprefix.list</name>
+        <value>${RUBIX_CACHE_DIR}/data</value>
+    </property>
+    <property>
+        <name>fs.rubix.impl</name>
+        <value>com.qubole.rubix.presto.CachingPrestoS3FileSystem</value>
+    </property>
+    <property>
+        <name>fs.wasb.impl</name>
+        <value>com.qubole.rubix.presto.CachingPrestoNativeAzureFileSystem</value>
+    </property>
+    <property>
+        <name>caching.fs.presto-server-port</name>
+        <value>8889</value>
+    </property>
+
+CLIENT
+) > ${RUBIX_PRESTO_CLIENT_CONFIG}
+
+grep --no-group-separator -a2 "yarn.resourcemanager.address" ${HADOOP_HOME_DIR}/etc/hadoop/yarn-site.xml >> ${RUBIX_PRESTO_CLIENT_CONFIG}
+echo "</configuration>" >> ${RUBIX_PRESTO_CLIENT_CONFIG}
+
+# Configure Presto Server to include RubiX client configuration as resource
+PRESTO_HIVE_CONFIG=/etc/presto/conf/catalog/hive.properties
+HIVE_RESOURCES="hive\.config\.resources"
+sed -i "/^${HIVE_RESOURCES}/ s|$|,${RUBIX_PRESTO_CLIENT_CONFIG}|" ${PRESTO_HIVE_CONFIG}
+
+# Copy RubiX JARs to Presto plugin directory
+cp -a ${RUBIX_HOME_DIR}/lib/* ${PRESTO_HOME_DIR}/plugin/hive-hadoop2/
+
+restart presto-server
---
+++ rubix-rpm/src/main/resources/rpm/scripts/configure-spark.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+RUBIX_HOME_DIR=/usr/lib/rubix
+
+# Configure Spark as RubiX client
+SPARK_DEFAULTS_CONF_FILE="/etc/spark/conf/spark-defaults.conf"
+SPARK_DRIVER_EXTRA_CP_OPTION="spark\.driver\.extraClassPath"
+SPARK_EXECUTOR_EXTRA_CP_OPTION="spark\.executor\.extraClassPath"
+RUBIX_EXTRA_CP=":${RUBIX_HOME_DIR}/lib/*"
+JETS3T_EXTRA_CP=":/usr/lib/hadoop/lib/jets3t-0.9.0.jar"
+sed -i "/^$SPARK_DRIVER_EXTRA_CP_OPTION/ s|$|$RUBIX_EXTRA_CP|" ${SPARK_DEFAULTS_CONF_FILE}
+sed -i "/^$SPARK_DRIVER_EXTRA_CP_OPTION/ s|$|$JETS3T_EXTRA_CP|" ${SPARK_DEFAULTS_CONF_FILE}
+sed -i "/^$SPARK_EXECUTOR_EXTRA_CP_OPTION/ s|$|$RUBIX_EXTRA_CP|" ${SPARK_DEFAULTS_CONF_FILE}
+sed -i "/^$SPARK_EXECUTOR_EXTRA_CP_OPTION/ s|$|$JETS3T_EXTRA_CP|" ${SPARK_DEFAULTS_CONF_FILE}
+
+echo "spark.hadoop.fs.s3.impl   com.qubole.rubix.hadoop2.CachingNativeS3FileSystem" >> ${SPARK_DEFAULTS_CONF_FILE}
+echo "spark.hadoop.fs.s3n.impl  com.qubole.rubix.hadoop2.CachingNativeS3FileSystem" >> ${SPARK_DEFAULTS_CONF_FILE}
+echo "spark.hadoop.fs.s3a.impl  com.qubole.rubix.hadoop2.CachingS3AFileSystem" >> ${SPARK_DEFAULTS_CONF_FILE}
+echo "spark.hadoop.fs.wasb.impl com.qubole.rubix.hadoop2.CachingNativeAzureFileSystem" >> ${SPARK_DEFAULTS_CONF_FILE}
