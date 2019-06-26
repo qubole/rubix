e2f4d8eed60733a10af42eae4abef238ba43aff1
Jordan Wilson <jordanw@bitquilltech.com>
2019-06-26 12:29:33 -0700

    timeout investigation - S3 FileSystem slowdown

--- rubix_travis_before_install.sh
+++ rubix_travis_before_install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash -e
 
-docker build --tag=rubix-build .
+echo "Building RubiX build image"
+#docker pull quboleinc/hadoop_mvn_thrift
+docker build --tag=rubix-build . > docker-rubix-build.log 2>&1
+echo "Finished building image"
--- rubix_travis_script.sh
+++ rubix_travis_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD":/usr/src/rubix -v "$HOME/.m2":/root/.m2 $ci_env -w /usr/src/rubix quboleinc/hadoop_mvn_thrift /bin/bash -c "./script.sh ; tail -f /dev/null"
+docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD":/usr/src/rubix -v "$HOME/.m2":/root/.m2 $ci_env -w /usr/src/rubix rubix-build /bin/bash -c "./script.sh"
