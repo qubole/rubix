9f6aaa2bf2d58aa712a080f45b9ad2ec8d9db994
Łukasz Osipiuk <lukasz@osipiuk.net>
2020-05-05 15:40:45 +0200

    Add Presto caching wrappers for all Azure filesystems

---
+++ rubix-prestosql/src/main/java/com/qubole/rubix/prestosql/CachingPrestoSecureAzureBlobFileSystem.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2019. Qubole Inc
+ * Licensed under the Apache License, Version 2.0 (the License);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an AS IS BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. See accompanying LICENSE file.
+ */
+package com.qubole.rubix.prestosql;
+
+import com.qubole.rubix.core.CachingFileSystem;
+import com.qubole.rubix.core.ClusterManagerInitilizationException;
+import com.qubole.rubix.spi.ClusterType;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.azurebfs.AzureBlobFileSystem;
+import org.apache.hadoop.fs.azurebfs.SecureAzureBlobFileSystem;
+
+import java.io.IOException;
+import java.net.URI;
+
+public class CachingPrestoSecureAzureBlobFileSystem extends CachingFileSystem<SecureAzureBlobFileSystem>
+{
+  private static final String SCHEME = "abfss";
+
+  public CachingPrestoSecureAzureBlobFileSystem()
+  {
+    super();
+  }
+
+  @Override
+  public void initialize(URI uri, Configuration conf) throws IOException
+  {
+    try {
+      initializeClusterManager(conf, ClusterType.PRESTOSQL_CLUSTER_MANAGER);
+      super.initialize(uri, conf);
+    }
+    catch (ClusterManagerInitilizationException ex) {
+      throw new IOException(ex);
+    }
+  }
+
+  public String getScheme()
+  {
+    return SCHEME;
+  }
+}
