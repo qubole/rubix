bb22e730eca8bfdc8a92569280e350e7f8d1ca77
James Taylor <jamestaylor@lyft.com>
2020-10-07 10:15:45 -0700

    Expand wildcard exports

--- rubix-prestosql/src/test/java/com/qubole/rubix/prestosql/TestingNodeManager.java
+++ rubix-prestosql/src/test/java/com/qubole/rubix/prestosql/TestingNodeManager.java
@@ -28,8 +28,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
-import java.net.*;
-import java.util.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 
 import static java.util.Objects.requireNonNull;
 
